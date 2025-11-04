f2742489726481f32db1f7cb2455231d5becafa3
hanssmagnum <Juan.molina.grisales@correounivalle.edu.co>
2025-11-04 14:39:14 -0500

    feature: add roles and new token item

--- src/routes/events.js
+++ src/routes/events.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { createEvent, getAllEvents, joinEvent } = require("../controllers/eventController");
 const { verifyToken } = require("../middleware/authMiddleware");
+const { checkRole } = require("../middleware/checkroleMiddleware");//se trae la funcion para verificar el rol
 
-router.post("/create", verifyToken, createEvent);
+router.post("/create", verifyToken,checkRole(["2","3"]), createEvent); //se incluye la funcion de verificar si cumple deja continuar con la funcion
 router.get("/all", verifyToken, getAllEvents);
 router.post("/join", verifyToken, joinEvent);
 
