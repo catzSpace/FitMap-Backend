1eb3ac7b8ad4f9923b2798da079e35d1a45f4937
hanssmangnum <juan.molina.grisales@correounivalle.edu.co>
2025-11-04 14:56:15 -0500

    Revert "feature: add roles and new token item"

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,17 +15,15 @@ async function registerUser(req, res) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const rolId = 1; //rol 1 visitante
-
     const [result] = await db.query(
-      `INSERT INTO usuarios (nombres, apellidos, cedula, email, password, fecha_nacimiento,rol_id)
-       VALUES (?, ?, ?, ?, ?, ?, ?)`,
-      [nombres, apellidos, cedula, email, hashedPassword, fechaNacimiento, rolId]
+      `INSERT INTO usuarios (nombres, apellidos, cedula, email, password, fecha_nacimiento)
+       VALUES (?, ?, ?, ?, ?, ?)`,
+      [nombres, apellidos, cedula, email, hashedPassword, fechaNacimiento]
     );
 
-    const token = jwt.sign({ id: result.insertId, role: rolId}, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-    res.status(201).json({ userId: result.insertId, role: rolId, token });
+    res.status(201).json({ userId: result.insertId, token });
   } catch (error) {
     console.error("Error al crear usuario:", error);
     res.status(500).json({ error: "Error en el servidor" });
@@ -47,9 +45,9 @@ async function loginUser(req, res) {
     if (!isMatch)
       return res.status(401).json({ error: "Credenciales incorrectas" });
 
-    const token = jwt.sign({ id: user.id, role: user.rol_id }, process.env.JWT_SECRET, { expiresIn: "7d" });//se agrego el rol
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-    res.json({ userId: user.id, role: user.rol_id, token });//se agrego el rol traido del usuario
+    res.json({ userId: user.id, token });
   } catch (error) {
     console.error("Error en login:", error);
     res.status(500).json({ error: "Error en el servidor" });
